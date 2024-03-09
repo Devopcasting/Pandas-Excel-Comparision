e2239412e18ba97afc71de1a138c78b60e4568c4
devopcasting <devopcasting@gmail.com>
2024-03-09 20:00:32 +0530

    added route for excel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import route_excel
+from app.routes import route_excel, route_docx
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 
@@ -8,6 +8,7 @@ app.mount("/static", StaticFiles(directory=r"app\static"), name="static")
 
 # Include routes
 app.include_router(route_excel.excel_router)
+app.include_router(route_docx.docx_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
